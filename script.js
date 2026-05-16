bf107eedc3484328d5a2272905b7d27689c131d7
madakavarunkumar <madakavarunkumar8@gmail.com>
2026-05-16 22:35:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const sheetID = "1EKM11SlVZV8WnXFuc8a5gxaB3ccQN8u_z7b8ExvyKeg";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDb5iLgpVampkzP1G2Y1lLEZDwEw0dhYUM",
+  authDomain: "sai-lakshmi-cafe.firebaseapp.com",
+  projectId: "sai-lakshmi-cafe",
+  storageBucket: "sai-lakshmi-cafe.firebasestorage.app",
+  messagingSenderId: "602856320749",
+  appId: "1:602856320749:web:b60b9af0f9e0aa7e2aa6e4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected");const sheetID = "1EKM11SlVZV8WnXFuc8a5gxaB3ccQN8u_z7b8ExvyKeg";
 
 const url = `https://opensheet.elk.sh/${sheetID}/Sheet1`;
 
